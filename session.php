62240cde0f920cd333ab7ccba9d68404b9cf1b4b
Bangio19 <bparamburu@gmail.com>
2020-04-17 17:44:58 +0200

    reorganisation des fichiers

--- session.php
+++
@@ -1,7 +0,0 @@
-<?php
-//Cette page vérifie si l'user est connecté pour des raisons de sécurité,
-	//nous le renvoyons à l'accueil si ce n'est pas le cas
-	if (!isset($_SESSION['user_id'])) {
-		header('Location: Accueil.php');
-	}
-?>
