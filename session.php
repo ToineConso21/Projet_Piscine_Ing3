6166df99f4ab1dae2b3c7f78045356a0a950d1f8
Bangio19 <bparamburu@gmail.com>
2020-04-17 16:05:08 +0200

    Sessions fonctionnelles sur Accueil, Connexion, les comptes, gestion du logout + icone logout

---
+++ session.php
@@ -0,0 +1,7 @@
+<?php
+//Cette page vérifie si l'user est connecté pour des raisons de sécurité,
+	//nous le renvoyons à l'accueil si ce n'est pas le cas
+	if (!isset($_SESSION['user_id'])) {
+		header('Location: Accueil.php');
+	}
+?>
